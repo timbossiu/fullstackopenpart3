0704ffa0663bb70e2ec73b1cd6592c3be7b6667a
Tim Alderkot <timalderkot@gmail.com>
2025-01-25 16:15:08 +0100

    done exercises 3.7 3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('body', function(req, res) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time :body'))
 
 let persons = [
     { 
