4476e828ced2db70ac6ff38c3daff17188fcfaa1
Tim Alderkot <timalderkot@gmail.com>
2025-03-30 20:01:00 +0200

    made 3.15 - 3.18

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ morgan.token('body', function(req, res) {
     return JSON.stringify(req.body)
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 const PORT = process.env.PORT || 3001;
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time :body'))
@@ -26,15 +36,19 @@ app.get('/api/persons', (request, response) => {
       })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
         response.json(person)
-      })
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p> 
-    <p>${new Date(Date.now())}</p>`)
+  Person.countDocuments().then(number => 
+    response.send(`<p>Phonebook has info for ${number} people</p> 
+      <p>${new Date(Date.now())}</p>`)
+  )
 })
 
 app.post('/api/persons', (request, response) => {
@@ -54,12 +68,35 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.json(persons)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+app.use(errorHandler)
