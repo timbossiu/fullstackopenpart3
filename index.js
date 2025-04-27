96d9d1559677cf6ada211bbb215a71cd6caefabc
Tim Alderkot <timalderkot@gmail.com>
2025-04-27 14:00:26 +0200

    updated frontend and error handling for validationErrors

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   } 
 
   next(error)
@@ -51,7 +54,7 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
   if (!body.name || !body.number) {
@@ -63,9 +66,11 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
+  person.save()
+  .then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
